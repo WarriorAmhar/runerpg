77d0c30335c069325c4ee8d817487b9637ad320b
WarriorAmhar <96066905+WarriorAmhar@users.noreply.github.com>
2025-12-16 04:13:36 -0300

    Delete runerpg.mjs

--- runerpg.mjs
+++
@@ -1,8 +0,0 @@
-Hooks.once("init", () => {
-  console.log("Rune RPG system initializing");
-
-  // Register actor types explicitly
-  CONFIG.Actor.typeLabels = {
-    character: "Character"
-  };
-});
